3bbf0bacc68c868ebc329d741517fa44796c2957
Federico Gentili <federicogentili05@gmail.com>
2026-04-16 14:39:10 +0200

    Add manifest.js for application metadata

---
+++ app/manifest.js
@@ -0,0 +1,13 @@
+import site from './site';
+
+export default function manifest() {
+  return {
+    name: site.name,
+    short_name: site.name,
+    description: site.description,
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+  };
+}
